91016a41ab482ddf6ae1d2eb85183de73f0ca57e
Nhat Minh <phannhatminh1099@example.com>
2023-06-16 10:35:34 +0700

    add Search and update DB

--- components/Board.tsx
+++ components/Board.tsx
@@ -6,10 +6,11 @@ import Column from './Column';
 
 
 function Board() {
-  const [board, getBoard, setBoardState] = useBoardStore((state) => [
+  const [board, getBoard, setBoardState, updateTodoInDB] = useBoardStore((state) => [
     state.board, 
     state.getBoard, 
-    state.setBoardState
+    state.setBoardState,
+    state.updateTodoInDB,
   ]);
   useEffect(()=>{
     getBoard();
@@ -77,6 +78,8 @@ function Board() {
         id: finishCol.id,
         todos: finishTodos,
       });
+
+      updateTodoInDB(todoMoved, finishCol.id)
 
       setBoardState({...board, columns: newColumns});
     }
--- store/BoardStore.ts
+++ store/BoardStore.ts
@@ -1,20 +1,33 @@
+import { db } from '@/firebase/clientApp';
 import { Board, Column, Todo, TypedColumns } from '@/typing';
 import { getTodosGroupedByColum } from '@/utils/getTodosGroupedByColumn';
+import { doc, serverTimestamp, updateDoc } from 'firebase/firestore';
 import { create } from 'zustand';
 
 interface BoardState {
     board: Board;
     getBoard:() => void;
     setBoardState: (board: Board) => void;
-    // updateTodoInDB: (todo: Todo, columnId: TypedColumns) => void;
+    updateTodoInDB: (todo: Todo, columnId: TypedColumns) => void;
+    searchString: string;
+    setSearchString: (searchString: string) => void;
 }
 export const useBoardStore = create<BoardState>((set) => ({
     board: {
         columns: new Map<TypedColumns, Column>()
     },
+    searchString:"",
+    setSearchString: (searchString) => set({ searchString }),
     getBoard: async () => {
         const board = await getTodosGroupedByColum();
         set({ board });
     },
-    setBoardState: (board) => set({ board })
+    setBoardState: (board) => set({ board }),
+    updateTodoInDB: async (todo, columnId) => {
+        const todoRef = doc(db, "todos", todo.id);
+        await updateDoc(todoRef,{
+            updatedAt: serverTimestamp(),
+            status: columnId
+        });
+    },
 }))
